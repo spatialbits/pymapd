7a73aa19dc18d2eff97f88a42051b9473bc35821
adam <adam@bitspatial.com>
2020-02-09 11:18:40 -0800

    pip upgrade for pyarrow to run in colab

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = ['thrift == 0.11.0',
                     'sqlalchemy >= 1.3',
                     'numpy >= 1.16',
                     'pandas >= 0.24',
-                    'pyarrow >= 0.12.0,<0.14',
+                    'pyarrow >= 0.12.0,<=0.14.1',
                     'packaging >= 19.0',
                     'rbc-project']
 
